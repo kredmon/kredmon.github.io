7508cf46f9823a5883915c0aed4689489046379a
Kevin Redmon <kcredmon@gmail.com>
2023-08-09 16:56:41 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-document.getElementById('date').innerHTML = "test1" + new Date().toDateString() + window.location.href;
+document.getElementById('date').innerHTML = new Date().toDateString() + window.location.href;
 console.log("Kevin " + "is a" + "Cool Guy!");
 // const currentUrl = window.location.href;
 // console.log(currentUrl);
