44658de67201de840b56e8bc078f4adee9369827
Kevin Redmon <kcredmon@gmail.com>
2023-08-09 16:53:19 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,3 @@
-document.getElementById('date').innerHTML = new Date().toDateString() + window.location.href;
+document.getElementById('date').innerHTML = "test1" + new Date().toDateString() + window.location.href;
 const currentUrl = window.location.href;
 console.log(currentUrl);
